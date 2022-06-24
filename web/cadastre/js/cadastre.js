01c50fc5a38875d05a05b217cc5c7f72d40edcfb
RaulShinaede <82043220+RaulShinaede@users.noreply.github.com>
2022-06-24 20:33:30 -0300

    Alterando apresentação da página de cadastro

--- web/cadastre/js/cadastre.js
+++ web/cadastre/js/cadastre.js
@@ -1,11 +1,40 @@
-function cadastre() {
+async function sleep(msec) {
+    return new Promise(resolve => setTimeout(resolve, msec));
+}
+
+async function showAlert() {
+    let element = document.getElementById('top-msg-container');
+    element.style.visibility = 'visible';
+
+    await sleep(1000);
+
+    element.style.visibility = 'hidden';
+}
+
+function cadastre(event) {
+
+    event.preventDefault();
 
     const nameInput = document.getElementById('name');
     const emailInput = document.getElementById('email');
     const passwordInput = document.getElementById('password');
 
     Security.createUser(nameInput.value, passwordInput.value, emailInput.value);
+
+    showAlert();
+
+    event.initEvent()
 }
 
-let element = document.getElementById('create-account-button');
-element.addEventListener('click', cadastre);
+function addEventListeners() {
+    let element = document.getElementById('top-msg-close-btn');
+    element.addEventListener('click', () => {
+        let inner = document.getElementById('top-msg-container');
+        inner.style.visibility = 'hidden';
+    });
+}
+
+let element = document.getElementById('cadastre-form-obj');
+element.addEventListener('submit', cadastre);
+
+addEventListeners();
